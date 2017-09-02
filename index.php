a490a55287d2f0952b261f667b7ea977d83bec7d
Kenny Ray <kenny@kennyray.com>
2017-09-02 14:49:13 -0400

    Updated license. Made variables unique to avoid collisions

--- index.php
+++ index.php
@@ -6,27 +6,34 @@ Description: Allows you to add Google Analytics to your blog
 Version: 1.0
 Author: Kenny Ray
 Author URI: http://phpblog.kennyray.com
-License: GPL2
+License: MIT
 */
  
-/*  Copyright 2017  Kenny Ray  (email : kenny@kennyray.com)
- 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
- 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
- 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+/*  MIT License
+
+    Copyright (c) 2017 Kenny Ray
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
-define( 'DIR_PATH', plugin_dir_path( __FILE__ ) );
-define( 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'JGA_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'JGA_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 class JustGoogleAnalytics
 {
@@ -41,8 +48,8 @@ class JustGoogleAnalytics
     public function init()
     {
         add_action('admin_menu', array($this, 'add_menu_item'));
-        register_activation_hook( DIR_PATH,  array( 'JustGoogleAnalytics', 'activate' ) );
-        register_deactivation_hook( DIR_PATH,  array( 'JustGoogleAnalytics', 'deactivate' ) );
+        register_activation_hook( JGA_DIR_PATH,  array( 'JustGoogleAnalytics', 'activate' ) );
+        register_deactivation_hook( JGA_DIR_PATH,  array( 'JustGoogleAnalytics', 'deactivate' ) );
 
 
 
@@ -118,7 +125,7 @@ class JustGoogleAnalytics
     public function AddGACodeToHead()
     {   $jga_code = $this->GetGACode();
         if($jga_code) {
-            wp_enqueue_script('jga-gacodescript', PLUGIN_URL . 'jga.js', array(), '1.0');
+            wp_enqueue_script('jga-gacodescript', JGA_PLUGIN_URL . 'jga.js', array(), '1.0');
             wp_add_inline_script('jga-gacodescript', ' (function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
